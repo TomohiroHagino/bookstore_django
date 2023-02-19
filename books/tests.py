ee0774f4a223f3acb771b04ace8ab87352c3144e
Hagi T <marspeoplehg@gmail.com>
2023-02-19 22:21:38 +0400

    ch12

--- books/tests.py
+++ books/tests.py
@@ -1,20 +1,31 @@
 # books/tests.py
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse 
 
-from .models import Book 
+from .models import Book, Review
 
 
 class BookTests(TestCase): 
     @classmethod
-    def setUpTestData(cls):cls.book = Book.objects.create( 
-        title="Harry Potter",
-        author="JK Rowling",
-        price="25.00",
-    )
-
-    def test_book_listing(self):
-        self.assertEqual(f"{self.book.title}", "Harry Potter") 
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user( # new 
+            username="reviewuser",
+            email="reviewuser@email.com",
+            password="testpass123",
+        ) 
+        cls.book = Book.objects.create(
+            title="Harry Potter",
+            author="JK Rowling",
+            price="25.00",
+        )
+        cls.review = Review.objects.create(  # new
+            book=cls.book,
+            author=cls.user,
+            review="An excellent review",
+        )
+    def test_book_listing(self): 
+        self.assertEqual(f"{self.book.title}", "Harry Potter")
         self.assertEqual(f"{self.book.author}", "JK Rowling")
         self.assertEqual(f"{self.book.price}", "25.00")
 
@@ -22,7 +33,7 @@ class BookTests(TestCase):
         response = self.client.get(reverse("book_list"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Harry Potter")
-        self.assertTemplateUsed(response, "books/book_list.html") 
+        self.assertTemplateUsed(response, "books/book_list.html")
 
     def test_book_detail_view(self):
         response = self.client.get(self.book.get_absolute_url())
@@ -30,4 +41,5 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Harry Potter")
+        self.assertContains(response, "An excellent review")
         self.assertTemplateUsed(response, "books/book_detail.html")
